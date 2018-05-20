aa46875c2a1310efb5b9bd477772e11a8bc99432
Matt Stofko <matt@mjslabs.com>
2018-05-20 14:21:22 -0700

    Clean up syntax, check for hashed known_hosts

--- custom-shared/plugins/ssh-autocomplete/ssh-autocomplete.plugin.zsh
+++ custom-shared/plugins/ssh-autocomplete/ssh-autocomplete.plugin.zsh
@@ -1,18 +1,25 @@
-local PROGGIES='(ssh|scp|sftp|slogin)'
+local PROGGIES='(ssh|scp|sftp|slogin|rsync)'
+local KNOWN_HOSTS=~/.ssh/known_hosts
+local SSH_CONFIG=~/.ssh/config
 
 # Don't add users in the autocomplete list, .ssh/config should know who we want to ssh as
 zstyle ':completion:*:'${PROGGIES}':*' users 
 
 # Modified version of https://serverfault.com/a/170481
-h=()
-if [[ -r ~/.ssh/config ]]; then
-  h=($h ${${${(@M)${(f)"$(cat ~/.ssh/config)"}:#Host *}#Host }:#*[*?]*})
+local autohosts=()
+if [[ -r $SSH_CONFIG ]]; then
+  autohosts=($autohosts ${${${(@M)${(f)"$(cat ${SSH_CONFIG})"}:#Host *}#Host }:#*[*?]*})
 fi
 
-if [[ -r ~/.ssh/known_hosts ]]; then
-  h=($h ${${${(f)"$(cat ~/.ssh/known_hosts{,2} || true)"}%%\ *}%%,*}) 2>/dev/null
+if [[ -r $KNOWN_HOSTS ]]; then
+  if [[ $(grep -c '^|' $KNOWN_HOSTS) -gt 0 ]]; then
+    printf "${WHITE}*** `basename ${(%):-%x}` ***${RESET}\n"
+    printf "${RED}[ERROR]${RESET} Not loading hashed ${KNOWN_HOSTS}\n\n"
+  else
+    autohosts=($autohosts ${${${(f)"$(cat $KNOWN_HOSTS || true)"}%%\ *}%%,*}) 2>/dev/null
+  fi
 fi
 
-if [[ $#h -gt 0 ]]; then
-  zstyle ':completion:*:'${PROGGIES}':*' hosts $h
+if [[ $#autohosts -gt 0 ]]; then
+  zstyle ':completion:*:'${PROGGIES}':*' hosts $autohosts
 fi
