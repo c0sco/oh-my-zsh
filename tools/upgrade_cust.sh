a338cd7db83e6798b2aac5baaf4c5657c5d08c47
Matt Stofko <matt@mjslabs.com>
2018-05-20 15:10:35 -0700

    Update submodules

--- tools/upgrade_cust.sh
+++ tools/upgrade_cust.sh
@@ -2,11 +2,22 @@
 
 git pull
 git pull git@github.com:robbyrussell/oh-my-zsh.git master
+
 git submodule init
 git submodule update
+for dir in ${ZSH}/custom-shared/plugins/*; do
+  if [[ -e ${dir}/.git ]]; then
+    pushd $dir &>/dev/null
+    echo Updating $dir
+    git pull origin master
+    popd &>/dev/null
+  fi
+done
+
 git status
+
 echo
 echo "If you're happy with what you see, run "
 echo
-echo "git push"
+echo "  ${WHITE}git commit -am 'Updating all' && git push${RESET}"
 echo
